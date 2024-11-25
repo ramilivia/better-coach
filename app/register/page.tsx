6624f4693bcdef857023e28a5697a8ff39eae5aa
Rodrigo Amilivia <r.amilivia@gmail.com>
2024-11-25 23:25:22 +0100

    Abstracted Auth Form

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,24 +1,13 @@
 "use client";
 
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { useMutation } from "@apollo/client";
+import { AuthForm } from "@/components/auth-form";
 import { CreateUserDocument, CreateUserMutation } from "@/lib/graphql/generated/graphql";
-import { useState } from "react";
+import { useMutation } from "@apollo/client";
 import { useRouter } from "next/navigation";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { useState } from "react";
 
 export default function Register() {
   const router = useRouter();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const [error, setError] = useState<string | null>(null);
   
   const [createUser] = useMutation<CreateUserMutation>(CreateUserDocument, {
@@ -27,8 +16,7 @@ export default function Register() {
     }
   });
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+  const handleSubmit = async (email: string, password: string) => {
     setError(null);
     const { data } = await createUser({
       variables: {
@@ -40,54 +28,13 @@ export default function Register() {
   };
 
   return (
-    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <Card className="mt-8 mx-6 mx-auto max-w-sm pt-6 pb-6 border-white min-w-80">
-        <CardHeader>
-          <CardTitle className="text-2xl">Register</CardTitle>
-          <CardDescription className="text-white">
-            Enter your details below to create an account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          {error && (
-            <div className="text-red-500 text-sm mb-4">
-              {error}
-            </div>
-          )}
-          <form onSubmit={handleSubmit} className="grid gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                type="email"
-                className="w-full p-2 border rounded-md bg-transparent"
-                placeholder="name@example.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                id="password"
-                type="password"
-                className="w-full p-2 border rounded-md bg-transparent"
-                placeholder="Enter your password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </div>
-            <Button type="submit" className="w-full mt-4" variant="outline">
-              Register
-            </Button>
-          </form>
-          <CardDescription className="mt-10">
-            By clicking register you agree to our Terms of Service and Privacy Policy
-          </CardDescription>
-        </CardContent>
-      </Card>
-    </div>
-  )
+    <AuthForm
+      title="Register"
+      description="Enter your details below to create an account"
+      buttonText="Register"
+      error={error}
+      onSubmit={handleSubmit}
+      footer="By clicking register you agree to our Terms of Service and Privacy Policy"
+    />
+  );
 }
