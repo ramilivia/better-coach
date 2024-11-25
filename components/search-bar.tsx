2c21682ae9725f14b83806467aa82ab93ae75b1c
Rodrigo Amilivia <r.amilivia@gmail.com>
2024-11-25 21:16:40 +0100

    Close Icon

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -6,6 +6,7 @@ import { searchHomeProductsDocument } from "@/lib/graphql/queries/search-home-pr
 import { fetchGraphQL } from "@/lib/graphql/fetcher";
 import { P } from "./ui/typography";
 import Link from "next/link";
+import { X } from "lucide-react";
 
 export default function SearchBar() {
 
@@ -33,13 +34,29 @@ export default function SearchBar() {
     setSearchTerm(e.target.value);
   };
 
+  const clearSearch = () => {
+    setSearchTerm('');
+    setProducts([]);
+  };
+
   return(
     <div className="w-96 relative">
-      <Input 
-        placeholder="Search" 
-        onChange={onInputChange}
-        value={searchTerm}
-      />
+      <div className="relative">
+        <Input 
+          placeholder="Search" 
+          onChange={onInputChange}
+          value={searchTerm}
+          className="pr-8"
+        />
+        {searchTerm && (
+          <button
+            onClick={clearSearch}
+            className="absolute right-2 top-1/2 -translate-y-1/2 text-muted-foreground hover:text-foreground"
+          >
+            <X size={16} />
+          </button>
+        )}
+      </div>
       {products.length > 0 && (
         <ul className="absolute z-10 bg-background w-full border rounded-md p-3 mt-2">
           {products.map((product) => (
