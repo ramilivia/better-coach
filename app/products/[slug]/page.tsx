5adb358f22ac81e9e55affe2a2628d6a54ca4f9c
Rodrigo Amilivia <r.amilivia@gmail.com>
2024-11-26 01:14:48 +0100

    Accessing Slug with new format

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -1,10 +1,17 @@
-"use client";
+"use client"
 
+import { useParams } from "next/navigation";
 import { requireAuthentication } from "@/lib/require-authentication";
 
+type ProducDetalPageParams = {
+  slug: string
+}
+
 function ProductDetailPage() {
+  const { slug }: ProducDetalPageParams = useParams();
+  
   return (
-    <div>PRODUCT DETAIL PAGE</div>
+    <div>PRODUCT DETAIL PAGE: {slug}</div>
   );
 }
 
